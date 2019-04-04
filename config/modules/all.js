b679d48c7f95cba43b65e07d8bbb04e94e445f39
roman <shmoop207@gmail.com>
2019-04-04 12:55:10 +0300

    first

--- config/modules/all.js
+++ config/modules/all.js
@@ -2,7 +2,9 @@
 const mongo_1 = require("@appolo/mongo");
 const view_1 = require("@appolo/view");
 const validation_1 = require("@appolo/validation");
+const logger_1 = require("@appolo/logger");
 module.exports = async function (app, env) {
+    await app.module(logger_1.LoggerModule);
     await app.module(new view_1.ViewModule({ viewEngine: view_1.ViewEngines.nunjucks }));
     await app.module(validation_1.ValidationModule);
     await app.module(new mongo_1.MongoModule({ connection: env.mongo }));
--- config/modules/all.ts
+++ config/modules/all.ts
@@ -3,12 +3,15 @@ import { App } from "appolo";
 import { MongoModule } from "@appolo/mongo";
 import { ViewModule, ViewEngines } from "@appolo/view";
 import { ValidationModule } from "@appolo/validation";
+import { LoggerModule } from "@appolo/logger";
 
 export = async function(app: App, env: IEnv) {
+  await app.module(LoggerModule);
 
   await app.module(new ViewModule({ viewEngine: ViewEngines.nunjucks }));
 
   await app.module(ValidationModule);
+
 
   await app.module(new MongoModule({ connection: env.mongo }));
 }
